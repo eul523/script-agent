9b78b16fac369893fe262f80b89cfc7a7a8d5a42
eul <eyuelhaile@gmail.com>
2026-05-08 22:53:56 +0300

    finished the agents

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from typing import TypedDict, Annotated, Literal
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph
 from tavily import TavilyClient
 from dotenv import load_dotenv
-from langgraph.checkpoint.memory import MemorySaver
 from pydantic import BaseModel
 from prompts import RESEARCHER_PROMPT, CODE_WRITER_PROMPT, CODE_REVIEWER_PROMPT, CODE_FIXER_PROMPT
 
 _ = load_dotenv()
 search = TavilyClient()
-model = ChatOpenAI(model="llama-3.3-70b-versatile", base_url="https://api.groq.com/openai/v1", temperature=0.0)
+model = ChatOpenAI(temperature=0.0)
 
 class AgentState(TypedDict):
     problem: str 
@@ -36,21 +35,21 @@ def researcher_agent(state: AgentState):
         raise KeyError('The problem is not specified.')
     result = model.with_structured_output(ResearchOutput).invoke([SystemMessage(content=RESEARCHER_PROMPT), HumanMessage(content=problem)])
     
-    content = state['content'] or []
+    content = []
     for q in result['search_keywords']:
         res = search.search(query=q, max_results=1)
         content.append(res['results'][0]['content'])
     
-    return {'language': result['language'], 'content': '\n\n'.join(content)}
+    return {'language': result.language, 'content': state.get('content', '') + '\n\n' + '\n\n'.join(content)}
 
-def code_writter_agent(state: AgentState):
+def code_writer_agent(state: AgentState):
     problem = state['problem']
     lang = state['language']
     content = state['content']
     
     result = model.with_structured_output(CodeWriterOutput).invoke(CODE_WRITER_PROMPT.format(problem=problem, content=content, language=lang))
     
-    return {'script': result['code'], 'explanation': result['explanation']}
+    return {'script': result.code, 'explanation': result.explanation}
 
 def critique_agent(state: AgentState):
     script = state['script']
@@ -58,7 +57,7 @@ def critique_agent(state: AgentState):
     
     result = model.with_structured_output(CodeReviewerOutput).invoke(CODE_REVIEWER_PROMPT.format(problem=problem, code=script))
     
-    return {'critique': result['critique']}
+    return {'critique': result.critique}
 
 def code_fixer_agent(state: AgentState):
     critique = state['critique']
@@ -70,4 +69,29 @@ def code_fixer_agent(state: AgentState):
     
     result = model.with_structured_output(CodeWriterOutput).invoke(CODE_FIXER_PROMPT.format(problem=problem, language=language, research=content, critique=critique, code=code))
     
-    return {'script': result['code'], 'explanation': explanation}
+    return {'script': result.code, 'explanation': result.explanation}
+
+graph = StateGraph(AgentState)
+
+graph.add_node('researcher', researcher_agent)
+graph.add_node('code_writer', code_writer_agent)
+graph.add_node('critique', critique_agent)
+graph.add_node('code_fixer', code_fixer_agent)
+
+graph.set_entry_point('researcher')
+
+graph.add_edge('researcher', 'code_writer')
+graph.add_edge('code_writer', 'critique')
+graph.add_edge('critique', 'code_fixer')
+graph = graph.compile()
+if __name__ == '__main__':
+    prompt = input('Problem description: ')
+    while not prompt.strip():
+        print('Provide valid prompt.')
+        prompt = input('Problem description: ')
+    result = graph.invoke({
+        'problem': prompt
+    })
+    print(result['explanation'])
+    print('=================')
+    print(result['script'])
